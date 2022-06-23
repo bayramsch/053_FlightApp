8ea885c84e801f047cede07c8ec8c4d714733638
Bayramsch <bayramkmz7240@gmail.com>
2022-06-23 14:30:01 +0200

    Auth configration has made

---
+++ users/views.py
@@ -0,0 +1,25 @@
+from rest_framework.generics import CreateAPIView
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # self.perform_create(serializer)
+        user = serializer.save()
+        data = serializer.data
+        if Token.objects.filter(user=user).exists:
+            token = Token.objects.get(user=user)
+            data["token"] = token.key
+        else:
+            data["token"] = "No token createed for this user.."
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
